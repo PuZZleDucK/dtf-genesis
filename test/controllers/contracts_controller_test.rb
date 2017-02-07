54192bee4035f6041f24f5f66ba70e83e9131caa
PuZZleDucK <puzzleduck@gmail.com>
2017-02-08 04:36:27 +1100

    fix some, remove some and reverse others

--- test/controllers/contracts_controller_test.rb
+++ test/controllers/contracts_controller_test.rb
@@ -9,9 +9,9 @@ class ContractsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "non logged in user should get redirected" do
+  test "non logged in user should see contract list" do
     get contracts_path
-    assert_redirected_to new_user_session_path
+    assert_response :success
   end
 
 
